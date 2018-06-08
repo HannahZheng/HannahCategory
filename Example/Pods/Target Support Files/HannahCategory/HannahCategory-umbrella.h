ad20ad8861e18849e8a8690537de6d7ebf632ea8
郑晗 <zhenghan1221@163.com>
2018-06-08 15:15:22 +0800

    1.0.4 subspec目录问题

--- Example/Pods/Target Support Files/HannahCategory/HannahCategory-umbrella.h
+++ Example/Pods/Target Support Files/HannahCategory/HannahCategory-umbrella.h
@@ -21,16 +21,6 @@
 #import "UIButton+HHBadge.h"
 #import "UIImage+HHCompress.h"
 #import "UIView+HHLineDash.h"
-#import "UIColor+CrossFade.h"
-#import "UIColor+Hex.h"
-#import "UITableViewCell+HHRoundCorner.h"
-#import "UITableViewHeaderFooterView+HHRoundCorner.h"
-#import "UIView+HHRoundCorner.h"
-#import "NSObject+HHCurrentVC.h"
-#import "NSString+HHSize.h"
-#import "UIButton+HHBadge.h"
-#import "UIImage+HHCompress.h"
-#import "UIView+HHLineDash.h"
 
 FOUNDATION_EXPORT double HannahCategoryVersionNumber;
 FOUNDATION_EXPORT const unsigned char HannahCategoryVersionString[];
--- HannahCategory.podspec
+++ HannahCategory.podspec
@@ -8,7 +8,7 @@
 
 Pod::Spec.new do |s|
   s.name             = 'HannahCategory'
-  s.version          = '1.0.3'
+  s.version          = '1.0.4'
   s.summary          = '分类'
 
 # This description is used to generate tags and improve search results.
@@ -30,7 +30,7 @@ TODO: Add long description of the pod here.
 
   s.ios.deployment_target = '8.0'
 
-s.source_files = 'HannahCategory/Classes/**/*.{h,m}'
+s.source_files = 'HannahCategory/Classes/HannahCategory.h'
   
   # s.resource_bundles = {
   #   'HannahCategory' => ['HannahCategory/Assets/*.png']
